836181395e377c18d04bc969cd73fc610a0f557f
suemkwon <suekwon2002@gmail.com>
2024-12-05 11:55:45 -0700

    initial commit

---
+++ test/compojure_apiv2/core_test.clj
@@ -0,0 +1,7 @@
+(ns compojure-apiv2.core-test
+  (:require [clojure.test :refer :all]
+            [compojure-apiv2.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
