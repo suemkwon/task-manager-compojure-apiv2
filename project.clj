e02b03393c43318b26f32637b375c50e44ca1fa3
suemkwon <suekwon2002@gmail.com>
2024-12-05 11:57:19 -0700

    implemented swagger ui

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject compojure-apiv2 "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
-  :main ^:skip-aot compojure-apiv2.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+  :description "Simple Task Manager API"
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [metosin/compojure-api "2.0.0-alpha31"]
+                 [ring/ring-core "1.9.5"]
+                 [ring/ring-jetty-adapter "1.9.5"]
+                 [cheshire/cheshire "5.11.0"]]
+  :main compojure-apiv2.core
+  :source-paths ["src"]
+  :ring {:handler compojure-apiv2.core/app})
