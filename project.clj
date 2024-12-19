797721f934eb3cb7915556973c9f51106aa0ad25
suemkwon <suekwon2002@gmail.com>
2024-12-19 13:40:40 -0700

    added config.edn and clj hooks to resolve macros

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
                  [metosin/compojure-api "2.0.0-alpha31"]
                  [ring/ring-core "1.9.5"]
                  [ring/ring-jetty-adapter "1.9.5"]
-                 [cheshire/cheshire "5.11.0"]]
+                 [prismatic/schema "1.2.1"]
+                 [metosin/ring-http-response "0.9.3"]
+                 [cheshire "5.11.0"]]
   :main compojure-apiv2.core
   :source-paths ["src"]
   :ring {:handler compojure-apiv2.core/app})
